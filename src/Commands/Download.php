ebe301f7cad4bc7a8f9cd42beaa4d82e19c0fc7b
Andrey Helldar <helldar@ai-rus.com>
2022-06-04 01:23:27 +0300

    Prepared Download processor

--- src/Commands/Download.php
+++ src/Commands/Download.php
@@ -2,8 +2,13 @@
 
 namespace LaravelLang\StatusGenerator\Commands;
 
+use LaravelLang\StatusGenerator\Contracts\Processor;
+use LaravelLang\StatusGenerator\Processors\Download as DownloadProcessor;
+
 class Download extends Command
 {
+    protected string|Processor $processor = DownloadProcessor::class;
+
     protected function configure()
     {
         $this
---
+++ src/Processors/Download.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelLang\StatusGenerator\Processors;
+
+class Download extends Processor
+{
+    public function handle(): void
+    {
+        // TODO: Implement handle() method.
+    }
+}
