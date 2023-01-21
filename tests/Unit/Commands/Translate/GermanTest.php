3954c1adac0c7da7750fa2d402c04f967b54f0a9
Andrey Helldar <helldar@dragon-code.pro>
2023-01-21 23:43:46 +0300

    Fixed unstable test

--- tests/Unit/Commands/Translate/GermanTest.php
+++ tests/Unit/Commands/Translate/GermanTest.php
@@ -16,16 +16,23 @@ class GermanTest extends Base
 
     public function testPhp(): void
     {
-        $this->assertJsonFileEqualsJson([
-            '0'             => 'Numerische Null',
-            '10'            => 'Numerische Zehn',
-            '100'           => 'Numerisch Hundert',
-            'accepted'      => 'Die :attribute muss akzeptiert werden.',
-            'accepted_if'   => 'Die :attribute muss akzeptiert werden, wenn :other :value ist.',
-            'active_url'    => 'Die :attribute ist keine gültige URL.',
-            'between.array' => 'The :attribute must have between :min and :max items.',
-            'between.file'  => 'The :attribute must be between :min and :max kilobytes.',
-        ], 'locales/de/php.json', __FUNCTION__);
+        $values = $this->filesystem->load($this->tempPath('locales/de/php.json'));
+
+        $this->assertContainsEquals($values[0], ['Numerische Null']);
+        $this->assertContainsEquals($values[10], ['Numerische Zehn']);
+        $this->assertContainsEquals($values[100], ['Numerisch Hundert']);
+
+        $this->assertContainsEquals($values['accepted'], ['Die :attribute muss akzeptiert werden.', ':Attribute muss akzeptiert werden.']);
+
+        $this->assertContainsEquals(
+            $values['accepted_if'],
+            ['Die :attribute muss akzeptiert werden, wenn :other :value ist.', ':Attribute muss akzeptiert werden, wenn :other :value ist.']
+        );
+
+        $this->assertContainsEquals($values['active_url'], ['Die :attribute ist keine gültige URL.', ':Attribute ist keine gültige Internet-Adresse.']);
+
+        $this->assertContainsEquals($values['between.array'], ['The :attribute must have between :min and :max items.']);
+        $this->assertContainsEquals($values['between.file'], ['The :attribute must be between :min and :max kilobytes.']);
     }
 
     public function testPhpInline(): void
@@ -37,6 +44,20 @@ class GermanTest extends Base
             'between.array' => 'This field must have between :min and :max items.',
             'between.file'  => 'This field must be between :min and :max kilobytes.',
         ], 'locales/de/php-inline.json', __FUNCTION__);
+
+        $values = $this->filesystem->load($this->tempPath('locales/de/php-inline.json'));
+
+        $this->assertContainsEquals($values['accepted'], ['Dieses Feld muss akzeptiert werden.']);
+
+        $this->assertContainsEquals(
+            $values['accepted_if'],
+            ['Dieses Feld muss akzeptiert werden, wenn :other gleich :value ist.', 'Dieses Feld muss akzeptiert werden, wenn :other :value ist.']
+        );
+
+        $this->assertContainsEquals($values['active_url'], ['Dieses Feld ist keine gültige URL.', 'Das ist keine gültige Internet-Adresse.']);
+
+        $this->assertContainsEquals($values['between.array'], ['This field must have between :min and :max items.']);
+        $this->assertContainsEquals($values['between.file'], ['This field must be between :min and :max kilobytes.']);
     }
 
     public function testExcludes(): void
